acd560bed3d96f0337a51442cf4171ff60e4fed4
Shalini Ramchandani <shalini.ramchandani@usach.cl>
2018-10-17 03:15:03 -0300

    Swagger agregado. Probando

--- BACKEND/src/main/java/com/mongodb/mongodb/MongodbApplication.java
+++ BACKEND/src/main/java/com/mongodb/mongodb/MongodbApplication.java
@@ -2,13 +2,23 @@ package com.mongodb.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MongodbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbApplication.class, args);
 	}
 
+	@Bean
+	Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(ApiInfo.DEFAULT);
+	}
 
 }
